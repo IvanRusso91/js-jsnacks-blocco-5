8952d39a6066e4f188a3f96564d2b069fa2f95f1
IvanRusso91 <iv.russo91@gmail.com>
2022-04-28 19:04:39 +0200

    snack4 completato

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -5,3 +5,65 @@ Stampa a schermo la bici con peso minore utilizzando destructuring e template li
 */
 
 //--------------------------------------------------
+
+const biciCorsa = [
+  {
+    nome: 'bici1',
+    peso: 7,
+  },
+  {
+    nome: 'bici2',
+    peso: 10,
+  },
+  {
+    nome: 'bici3',
+    peso: 6.5,
+  },
+  {
+    nome: 'bici4',
+    peso: 6,
+  },
+  {
+    nome: 'bici5',
+    peso: 11,
+  },
+  {
+    nome: 'bici6',
+    peso: 6.8,
+  },
+]
+console.log(biciCorsa);
+
+const [bici1, bici2, bici3, bici4, bici5, bici6] = biciCorsa;
+
+// Test-----------
+// console.log(bici1);
+// console.log(bici2);
+ console.log(bici3.peso);
+// console.log(bici4);
+// console.log(bici5);
+// console.log(bici6);
+//-----------/Test.
+let pesoMinTemp = bici1.peso;
+
+console.log(pesoMinTemp);
+
+for(let bici of biciCorsa){
+
+
+  pesoMinTemp = (bici.peso < pesoMinTemp ) ? bici.peso : pesoMinTemp;
+
+  const output=
+  `
+  <div>
+    <ul>
+      <li>${bici.nome} : ${bici.peso}g</li>
+    </ul>
+  </div>
+  `;
+
+  document.querySelector('.stampa').innerHTML += output;
+  document.querySelector('.light').innerHTML = `La bici più leggera pesa: ${pesoMinTemp}`;
+}
+
+console.log('---',pesoMinTemp);
